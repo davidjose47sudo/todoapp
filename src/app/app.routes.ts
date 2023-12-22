70ac78df3a2451e0bf9c417c0565b95f0d84ad3c
David Silva <78758821+davidjose47sudo@users.noreply.github.com>
2023-12-22 14:21:19 -0500

    Update component styles and remove unused code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,10 @@ import { Routes } from '@angular/router';
 
 import {HomeComponent} from '../app/pages/home/home.component';
 import {LaboratorioComponent} from '../app/pages/laboratorio/laboratorio.component';
-import { PendientesComponent } from './pages/pendientes/pendientes.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'laboratorio', component: LaboratorioComponent },
-  { path: 'pending', component: PendientesComponent },
   { path: 'completed', component: LaboratorioComponent },
   { path: '**', redirectTo: 'home' }
 ];
